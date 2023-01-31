5fe34c15123c483fec536cb862d7cffdd6cc142d
ImBlakeHotz <79678872+ImBlakeHotz@users.noreply.github.com>
2023-01-31 10:32:54 -0500

    Initial commit

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1535755.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 102647270, 102647278, 102647280, 102647294, 102647297 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/102647263/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/102647264/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/102647262/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/102647267/1/basis.wasm.js', 'wasmUrl' : 'files/assets/102647268/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/102647266/1/basis.js', 'preload' : false},
+];
